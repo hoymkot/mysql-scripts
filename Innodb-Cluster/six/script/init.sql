77903ab0d19d602958b2dfc83936814393a18146
hoymkot <chrishouwakot@gmail.com>
2020-01-03 08:52:21 +0800

    test script for mysql innodb cluster

--- Innodb-Cluster/six/script/init.sql
+++
@@ -1,4 +0,0 @@
-CREATE USER 'user'@'%' IDENTIFIED BY 'user';
-GRANT ALL PRIVILEGES ON *.* TO 'user'@'%';
-GRANT GRANT OPTION ON *.* TO 'user'@'%';
-FLUSH PRIVILEGES
--- Innodb-Cluster/six/script/run.sh
+++
@@ -1,7 +0,0 @@
-docker run --name=$1 \
-   --mount type=bind,src=/docker/$1/data,dst=/var/lib/mysql \
-   --network mysql \
-   -e MYSQL_ROOT_PASSWORD=root \
-   -d mysql/mysql-server:latest \
-   -h $1
-
